ddcee60f6d5fe3744dcb210d94bf9e6524d443f4
Rinat Valiullov <4rontender@gmail.com>
2021-01-02 17:03:46 +0500

    add currencies blocks

--- project-01/src/App.js
+++ project-01/src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
+import currencies from "./currencies";
 
 class App extends Component {
   render() {
-    return <div>Hello!</div>;
+    return (
+      <div className="app">
+        <aside>
+          <ul className="currList">
+            {currencies.map((curr) => (
+              <li key={curr} className="currItem">
+                {curr}
+              </li>
+            ))}
+          </ul>
+        </aside>
+      </div>
+    );
   }
 }
 
