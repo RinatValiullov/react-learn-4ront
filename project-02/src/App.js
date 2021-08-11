008762bee21d47955608b83de6132bfd8d800bf6
Rinat Valiullov <4rontender@gmail.com>
2021-08-11 19:50:40 +0500

    basic App component

--- project-02/src/App.js
+++ project-02/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  let initialState = 0;
+  const [likes, setLikes] = useState(initialState);
+  const [value, setVlaue] = useState("Hello, React!");
+
+  function increment() {
+    setLikes(likes + 1);
+  }
+
+  function decrement() {
+    setLikes(likes - 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{likes}</div>
+      <div>{value}</div>
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => setVlaue(event.target.value)}
+      />
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
