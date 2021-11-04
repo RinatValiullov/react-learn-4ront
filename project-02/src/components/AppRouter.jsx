74c9f3eca292b6d13f07565cba292eb3d0bd07e7
Rinat Valiullov <4rontender@gmail.com>
2021-11-05 00:02:23 +0500

    refactor routing in "AppRouter" component (add router)

--- project-02/src/components/AppRouter.jsx
+++ project-02/src/components/AppRouter.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router';
-import { Error } from './../pages/Error';
-import { About } from './../pages/About';
-import { Posts } from './../pages/Posts';
-import { PostIdPage } from './../pages/PostIdPage';
+import { Redirect, Route, Switch } from 'react-router';
+import { routes } from './../router';
 
 const AppRouter = () => {
   return (
     <Switch>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route exact path="/posts">
-        <Posts />
-      </Route>
-      <Route exact path="/posts/:id">
-        <PostIdPage />
-      </Route>
-      <Route path="/error">
-        <Error />
-      </Route>
-      <Redirect to="/error" />
+      {
+        routes.map(route => {
+          return <Route
+            component={route.component}
+            path={route.path}
+            exact={route.exact}
+          />;
+        })
+      }
+      <Redirect to="/posts/" />
     </Switch>
   );
 };
