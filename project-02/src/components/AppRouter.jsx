55bb77c173545a6ccb026052dc6cbe75e32fed85
Rinat Valiullov <4rontender@gmail.com>
2021-11-07 21:38:34 +0500

    add logic to authorization form

--- project-02/src/components/AppRouter.jsx
+++ project-02/src/components/AppRouter.jsx
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
 import { privateRoutes, publicRoutes } from './../router';
+import { AuthContext } from './../context/index';
 
 const AppRouter = () => {
+  const { isAuth } = useContext(AuthContext);
+
+  console.log(isAuth);
+
   return (
-    <Switch>
-      {
-        privateRoutes.map(route => {
-          return <Route
-            component={route.component}
-            path={route.path}
-            exact={route.exact}
-          />;
-        })
-      }
-      {
-        publicRoutes.map(route => {
-          return <Route
-            component={route.component}
-            path={route.path}
-            exact={route.exact}
-          />;
-        })
-      }
-      <Redirect to="/posts/" />
-    </Switch>
+    isAuth
+      ? <Switch>
+        {
+          privateRoutes.map(route => {
+            return <Route
+              component={route.component}
+              path={route.path}
+              exact={route.exact}
+              key={route.path}
+            />;
+          })
+        }
+        <Redirect to="/posts" />
+      </Switch>
+      : <Switch>
+        {
+          publicRoutes.map(route => {
+            return <Route
+              component={route.component}
+              path={route.path}
+              exact={route.exact}
+              key={route.path}
+            />;
+          })
+        }
+        <Redirect to="/login" />
+      </Switch>
+
   );
 };
 
--- project-02/src/pages/Login.jsx
+++ project-02/src/pages/Login.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Input } from './../components/UI/input/Input';
 import { Button } from './../components/UI/button/Button';
+import { AuthContext } from './../context';
 
 const Login = () => {
+  const { isAuth, setIsAuth } = useContext(AuthContext);
+
+  const login = (event) => {
+    event.preventDefault();
+    setIsAuth(true);
+  };
   return (
     <div>
       <h1>Авторизоваться</h1>
-      <form>
+      <form onSubmit={login}>
         <Input type="text" placeholder="Введите логин" />
         <Input type="password" placeholder="Введите пароль" />
         <Button>Войти</Button>
