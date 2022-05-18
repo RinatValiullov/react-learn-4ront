9e0ccd31df0bd43f13157ce23df0fd3b6b41b99c
Rinat Valiullov <4rontender@gmail.com>
2022-05-19 04:59:24 +0500

    add "Pages" component

--- project-05/src/components/Pages.tsx
+++ project-05/src/components/Pages.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { PageTransition } from "@steveeeie/react-page-transition";
+import React, { useContext } from "react";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { pages } from "../pages";
+import { AppContext } from "./AppContext";
+import { Page } from "./Page";
 
 function Pages() {
-  return <div>Pages</div>;
+  const location = useLocation();
+  const appContext = useContext(AppContext);
+
+  if (appContext) {
+    return (
+      <PageTransition
+        preset={appContext.preset}
+        transitionKey={location.pathname}
+        enterAnimation={appContext.enterAnimation || ""}
+        exitAnimation={appContext.exitAnimation || ""}
+      >
+        <Routes location={location}>
+          {pages.map((page, index) => {
+            return (
+              <Route
+                key={index}
+                path={page.path}
+                element={<Page page={page} />}
+              />
+            );
+          })}
+          <Route path="/" element={<Navigate to="/page-1" />} />
+        </Routes>
+      </PageTransition>
+    );
+  }
+
+  return null;
 }
 
 export { Pages };
